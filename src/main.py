0079bb1578de00ed919f17b4b4e13a2a90b35307
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2021-12-10 10:50:42 +0000

    change

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ b=math.tan(3)
 z=complex(a,b)
 print("Complesso:", type(z),z)
 
-var1="fai cestil Giovanni!"
+var1="fai cestil Giovanni! "
 var2="stai per inciampare"
-print("Slicing:", var1[0:1])
+print("Slicing:", var1[0:10])
 print("Somma:", var1+var2)
