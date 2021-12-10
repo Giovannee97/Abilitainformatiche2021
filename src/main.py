6cd95a6ee75ea7f52b953d06da2abc675d246e67
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2021-12-10 11:05:47 +0000

    prova

--- src/main.py
+++ src/main.py
@@ -9,3 +9,18 @@ var1="fai cestil Giovanni! "
 var2="stai per inciampare"
 print("Slicing:", var1[0:10])
 print("Somma:", var1+var2)
+
+nome= "DOOM"
+
+string = " Gosh! %s!!! I should have brought my autograph book!" %nome
+print(string)
+adjective= "AWESOME"
+tool = "Flying guillotine"
+status = "dead"
+
+Bar1= " I been tossin, enforcing, my style is %s" %adjective
+
+Bar2= " ,I cause more family feuds than Richard Dawson"
+Bar3= " ,the survey says your %s" %status
+Bar4= " ,Fatal %s chops off your head " %tool
+print(Bar1+Bar2+Bar3+Bar4)
