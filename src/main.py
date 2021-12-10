0d9c714934827a320255759a69a538359aed4ed2
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2021-12-10 10:41:43 +0000

    abilità

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import cmath
+import math
+a=math.sin(2)
+b=math.tan(3)
+z=complex(a,b)
+print("Somplesso:", type(z),z)
+
+
+
+var1="fai cestil Giovanni "
+var2="stai per inciampare"
+print("Slicing:", var1[0:1])
+print("Somma:", var1+var2)
