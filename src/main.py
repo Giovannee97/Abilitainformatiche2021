7a0158bb1d38e0b8ca77b1151f13eb4bd7a7b265
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2021-12-10 10:44:16 +0000

    prova

--- src/main.py
+++ src/main.py
@@ -3,9 +3,7 @@ import math
 a=math.sin(2)
 b=math.tan(3)
 z=complex(a,b)
-print("Somplesso:", type(z),z)
-
-
+print("Complesso:", type(z),z)
 
 var1="fai cestil Giovanni "
 var2="stai per inciampare"
